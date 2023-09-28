4fc9d5d40739f0ad72db11e4cef4b5efdc9b123c
Mohamed Ibrahim <mohamed_im123@hotmail.com>
2023-09-28 22:32:53 +0100

    "Second commit"
    "This one uses Arrays to store the data and then implements a for loop to retrieve the numbers"

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,29 @@ public class Main {
                 + " whose number you wish to retrieve: ");
         String name = scanner.nextLine().toUpperCase().trim();
 
-        if (name.equals("BOB")) {
-            System.out.println("07970470470");
-        } else if (name.equals("SALLY")) {
-            System.out.println("07415118642");
-        } else {
+        String[] names = new String[3];
+        String[] number = new String[3];
+        names[0] = "BOB";
+        names[1] = "SALLY";
+        names[2] = "HODOR";
+        number[0] = "07970457470";
+        number[1] = "07415118642";
+        number[2] = "07903245098";
+
+        boolean found = false;
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals(name)) {
+                System.out.println("Number: " + number[i]);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
             System.out.println("This name does not exist");
         }
+
     }
 
 }
